e460316a03b9660f9abdcbe6dacd3da763c30d6c
Rodrigo04-py <rodrigosavio04@gmail.com>
2026-05-01 16:24:12 -0300

    Sistema de login feito - Rodrigo 010526

--- fazendaSertao.py
+++ fazendaSertao.py
@@ -12,6 +12,21 @@ while True:
     if opcao == 1:
         nome_usuario = input('Insira seu nome de usuario: ')
         senha_usuario = input('Insira sua senha: ')
+        logado = False
+
+        for adm in administrador:
+            if adm[0] == nome_usuario and adm[1] == senha_usuario:
+                print('Login realizado como ADM!')
+                logado = True
+        
+        for cli in cliente:
+            if cli[0] == nome_usuario and cli[1] == senha_usuario:
+                print('Login realizado como CLIENTE!')
+                logado = True
+        
+        if logado == False:
+            print('Usuario ou senha incorretos!')
+
     elif opcao == 2:
         print('Voce e ADM ou CLIENTE?')
         tipo_usuario = int(input('Digite 1- ADM ou 2- CLIENTE: '))
